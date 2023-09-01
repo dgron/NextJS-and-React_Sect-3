3731f9cc0494940358e06fb82597fd19406abd4a
Damian Gronsky <damian.gronsky@gmail.com>
2023-08-31 19:35:35 -0700

    Lecture 70 (finished)

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -5,9 +5,21 @@ function ClientProjectsPage() {
 
   console.log(router.query);
 
+  const loadProjectHandler = () => {
+    // load some data...
+    router.push({
+      pathname: "/clients/[id]/[clientprojectid]",
+      query: {
+        id: "max",
+        clientprojectid: "projecta",
+      },
+    });
+  };
+
   return (
     <div>
       <h1>The Projects of a Given Client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 }
